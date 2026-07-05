2598a15b7b552e53a142463e3513042feaa279c9
poisow12 <ikersipe12@gmail.com>
2026-07-05 11:28:13 +0000

    fix: add sameSite none for cross-domain cookies

--- artifacts/api-server/dist/index.mjs
+++ artifacts/api-server/dist/index.mjs
@@ -57881,6 +57881,7 @@ app.use(
     cookie: {
       httpOnly: true,
       secure: process.env["NODE_ENV"] === "production",
+      sameSite: process.env["NODE_ENV"] === "production" ? "none" : "lax",
       maxAge: 7 * 24 * 60 * 60 * 1e3
     }
   })
